0c4dd6f36ccfae27fe09141b04a2d800e069f625
Mark Aloo <mbaloo@safaricom.co.ke>
2024-05-30 18:23:42 +0300

    fix: update types

--- functions/serverUserFromSession.ts
+++ functions/serverUserFromSession.ts
@@ -1,13 +1,13 @@
 import { NextAuthOptions, Session, getServerSession } from "next-auth";
 
+type User = { username?: string; sub: string; userRegion: string };
+
 export default async function serverUserFromSession(
   authOptions: NextAuthOptions
-): Promise<{}> {
-  const session:
-    | ({
-        user: { username?: string; sub: string; userRegion: string };
-      } & Session)
-    | null = await getServerSession(authOptions);
+): Promise<User> {
+  const session: ({ user: User } & Session) | null = await getServerSession(
+    authOptions
+  );
 
   const username =
     session?.user?.sub ??
