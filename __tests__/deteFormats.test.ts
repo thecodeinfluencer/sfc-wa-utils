3a0f2bf240c3c07410cdfca9625a2e5066f9731a
Mark Aloo <mbaloo@safaricom.co.ke>
2024-05-30 16:32:07 +0300

    test: create new test

---
+++ __tests__/deteFormats.test.ts
@@ -0,0 +1,5 @@
+import { cleanFDate, inputFDate, serverFDate } from "../functions/dateFormat";
+
+console.log(cleanFDate("2024-05-30"));
+console.log(serverFDate("2024-05-30"));
+console.log(inputFDate("2024-05-30"));
--- test.ts
+++
@@ -1,5 +0,0 @@
-import { cleanFDate } from "./index";
-
-const date = cleanFDate("2023-04-15");
-
-console.log(date);
