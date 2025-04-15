36b7831d54deeb891832ef1484579b1161722a16
Mark Aloo <38157066+thecodeinfluencer@users.noreply.github.com>
2025-04-15 12:22:06 +0300

    feat: correct date format

--- functions/dateFormat.ts
+++ functions/dateFormat.ts
@@ -1,10 +1,10 @@
 import { format } from "date-fns";
 
 export const cleanFDate = (fDate: string): string =>
-  format(new Date(fDate), "MMM dd HH:MM");
+  format(new Date(fDate), "MMM dd HH:mm");
 
 export const serverFDate = (fDate: any): string =>
-  format(new Date(fDate), "yyyy-MM-dd HH:MM:SS");
+  format(new Date(fDate), "yyyy-MM-dd HH:mm:ss");
 
 export const inputFDate = (fDate: any): string =>
   format(new Date(fDate), "yyyy-MM-dd'T'hh:mm");
