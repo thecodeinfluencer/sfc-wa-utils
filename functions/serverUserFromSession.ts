50ec424414fddbc79347d37a4e7efb4e947a48ca
Mark Aloo <mbaloo@safaricom.co.ke>
2025-04-28 10:23:16 +0300

    fix: optional chaining in `getServerSession`

--- functions/serverUserFromSession.ts
+++ functions/serverUserFromSession.ts
@@ -3,5 +3,5 @@ import { NextAuthOptions, Session, getServerSession } from "next-auth";
 export default async function serverUserFromSession(
   authOptions: NextAuthOptions
 ): Promise<Session["user"]> {
-  return (await getServerSession(authOptions)).user;
+  return (await getServerSession(authOptions))?.user;
 }
